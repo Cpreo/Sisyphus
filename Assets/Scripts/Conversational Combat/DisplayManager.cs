45f66c6720ea8489f6e8e2d1f05247d69e25b689
Cpreo <68442127+Cpreo@users.noreply.github.com>
2023-08-07 15:00:53 -0400

    additions to Conversational Combat

--- Assets/Scripts/Conversational Combat/DisplayManager.cs
+++ Assets/Scripts/Conversational Combat/DisplayManager.cs
@@ -14,7 +14,7 @@ public class DisplayManager : MonoBehaviour
 
     private static DisplayManager instance;
     
-
+    [SerializeField] TextAsset inkJSON;
     public static DisplayManager GetInstance(){
         return instance;
     }
@@ -23,16 +23,41 @@ public class DisplayManager : MonoBehaviour
     // stores list of commands that are drawn so we know when to draw more!
     List<Command> drawnCommands;
     // Start is called before the first frame update
-
+    string interp_text;
     void Start()
     {
-        
+        // Ink Json asset should be stored somewhere else
+        Interpreter.GetInstance().LoadStory(inkJSON);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        interp_text = Interpreter.GetInstance().storyText;
+        //    Display.GetInstance().UpdateChat(interp_text);
+        // need section for log as well
+
+        // will need to change depending on mode
+        // note to self, should probabally do this stuff in phases so it is easier to test
+        // I remember coding games in elementary school and with Tarang, what a jungle...
+        if(Input.GetKeyDown(KeyCode.E) && Interpreter.GetInstance().continuing == false)
+        {
+            
+            // should probabally have a unified method for updating all elements
+            Interpreter.GetInstance().continuing = true;
+            
+            if(Interpreter.GetInstance().continuing)
+            {
+            Interpreter.GetInstance().ContinueStory();
+            string storyText = Interpreter.GetInstance().storyText;
+            Display.GetInstance().UpdateChat(storyText);
+            // Need to get speaker tags
+            Log.GetInstance().UpdateLog(Interpreter.GetInstance().storyText);
+            Display.GetInstance().DrawLog();
+            // We need to display the Log
+            }
+        }
+
     }
 
  
